dafd6036d64c3c02c5bd74d098cfdb424fe8b87e
Higor Marques <higor.marques@mindera.com>
2019-07-11 14:30:26 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+// Iniciando o App
+const app = express();
+
+// Iniciando o DB
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true
+});
+
+requireDir("./src/models");
+
+// rotas
+app.use("/api", require("./src/routes"));
+
+app.listen(3001);
